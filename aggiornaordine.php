cf2c97741c42273a588f2fd50a656fcefca0b008
Arianna Brivio <38939402+Aribrivio@users.noreply.github.com>
2018-09-19 18:37:49 +0200

    Add files via upload

---
+++ aggiornaordine.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+	<title>GamesCore</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="FogliStile/Struttura.css" type="text/css">
+	<link rel="stylesheet" href="FogliStile/Stile - contattaci.css" type="text/css">
+	<link rel="stylesheet" href="FogliStile/Menu1.css" type="text/css">
+	<link rel="stylesheet" href="FogliStile/Menu2.css" type="text/css">
+	<link rel="stylesheet" href="FogliStile/Accedi-Registrati.css" type="text/css">
+	<script type="text/javascript" src="engine0/jquery.js"></script>
+	
+		<?php
+			include("/Include/nero.php");
+			include("/Include/menu.php");
+			include("/Include/script.php");
+		?>
+	
+	</head>	
+		
+		
+<body>
+
+	<header>
+		
+	</header>
+	
+	<section id="ContenitoreSection" >
+	
+	$cwne= $_SESSION["IDCliente"]
+
+	</section>
+	
+		<footer style="padding-bottom:23px;">
+			<img src="Images/logofooter.jpg" Alt="Logo" Title="Logo">
+			<br> 
+			<a href=""><img id="icone" src="Images/facebook.png"></a>
+			<a href=""><img id="icone" src="Images/twitter.png"></a>
+			<a href=""><img id="icone" src="Images/instagram.jpg"></a>
+			<a href=""><img id="icone" src="Images/yt.png"></a>
+		</footer>
+</html>
